bba523d7b6e5284ef0ae32e0dc9b04ca50eac55d
Sean Ross-Ross <srossross@gmail.com>
2017-01-12 17:30:13 -0800

    updated script

--- stable_world/script.py
+++ stable_world/script.py
@@ -1,14 +1,44 @@
+"""
+This is a command
+"""
+from __future__ import print_function
 import click
+from stable_world import __version__ as sw_version
 
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def main(count, name):
+@click.group(invoke_without_command=True)
+@click.option('--debug/--no-debug', default=False)
+@click.version_option(sw_version)
+# @click.help_option('-h/--help')
+@click.pass_context
+def main(ctx, debug):
     """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+    print("ctx", ctx.invoked_subcommand)
+
+
+@main.command()
+def use():
+    """Simple program that greets NAME for a total of COUNT times."""
+
+
+@main.command()
+def success():
+    """Simple program that greets NAME for a total of COUNT times."""
+
+
+@main.command()
+def diff():
+    """Simple program that greets NAME for a total of COUNT times."""
+
+
+@main.command()
+def pin():
+    """Simple program that greets NAME for a total of COUNT times."""
+
+
+@main.command()
+def unpin():
+    """Simple program that greets NAME for a total of COUNT times."""
 
 
 if __name__ == '__main__':
