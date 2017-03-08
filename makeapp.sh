17779e98df18f8c6ad05a005780d727feb939c06
Sean Ross-Ross <srossross@gmail.com>
2017-03-08 08:07:14 -0800

    remove python2 testing for now

--- makeapp.sh
+++ makeapp.sh
@@ -1,27 +1,11 @@
 set -euo pipefail
 
 git clean -xdf dist app
-python setup.py sdist
+python3 setup.py sdist
 pip install --target app dist/stable.world-*.tar.gz
 
-if [ -z "$(which pyenv)" ]; then
-  if [ -z "$(which python2)" ]; then
-    echo "WARNING: python2 not installed"
-  else
-    python2 -m compileall app/
-  fi
+python2 -m compileall app/
+python3 -m compileall app/
 
-  if [ -z "$(which python3)" ]; then
-    echo "WARNING: python3 not installed"
-  else
-    python3 -m compileall app/
-  fi
-else
-  pyenv global 2.7.12
-  python -m compileall app/
-  pyenv global 3.6.0
-  python -m compileall app/
-fi
-
-python -m zipapp app --python "/usr/bin/env python"  -o ./bin/stable.world
+python3 -m zipapp app --python "/usr/bin/env python"  -o ./bin/stable.world
 chmod +x ./bin/stable.world
