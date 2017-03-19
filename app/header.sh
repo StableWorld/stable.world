5b757d10f69e7855ba1b9bec977d3d33f0f25d83
Sean Ross-Ross <srossross@gmail.com>
2017-03-18 20:37:50 -0700

    added debug output

--- app/header.sh
+++ app/header.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-if ! hash python; then
+# print when 'DEBUG' envvar is set
+decho () {
+  if [ ! -z "${DEBUG}" ]; then
+    echo $0 [$(date)] $*
+  fi
+}
+
+PYTHON="$(which python)"
+decho Found python $PYTHON
+if [ -z "${PYTHON}" ]; then
     echo "$0 requires python 2.7 or greater to be installed. Please install python"
     exit 1
 fi
 
-PY_VERSION="$(python --version 2>&1)"
+
+
+PY_VERSION="$($PYTHON --version 2>&1)"
+decho $PY_VERSION
+
 ver=$(echo $PY_VERSION | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
 if [ "$ver" -lt "27" ]; then
     echo "$0 requires python 2.7 or greater to be installed (got $PY_VERSION in $(which python))"
     exit 1
 fi
-
-python -suES "$0" "$@"
+decho running command $PYTHON -suES "$0" "$@"
+$PYTHON -suES "$0" "$@"
 
 exit $?
 
