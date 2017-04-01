e819e0747c4c4c44f42fe8dbd1a1f7031b7c592a
Sean Ross-Ross <srossross@gmail.com>
2017-04-01 09:28:59 -0700

    remove network tests

--- functional_tests/test_options.py
+++ functional_tests/test_options.py
@@ -1,18 +1,20 @@
-import os
-import unittest
-import pytest
-import requests
-from stable_world.client import Client
+# TODO: re-add when more stable
 
-STABLE_WORLD_URL = os.getenv('STABLE_WORLD_URL', 'http://probably.stable.world')
-url_templates = list(Client.url_templates().items())
-
-
-@pytest.mark.parametrize("url_template, test_method", url_templates)
-def test_client_urls(url_template, test_method):
-    res = requests.request(test_method, STABLE_WORLD_URL + url_template)
-    assert res.status_code == 200
-
-
-if __name__ == "__main__":
-    unittest.main()
+# import os
+# import unittest
+# import pytest
+# import requests
+# from stable_world.client import Client
+#
+# STABLE_WORLD_URL = os.getenv('STABLE_WORLD_URL', 'http://probably.stable.world')
+# url_templates = list(Client.url_templates().items())
+#
+#
+# @pytest.mark.parametrize("url_template, test_method", url_templates)
+# def test_client_urls(url_template, test_method):
+#     res = requests.request(test_method, STABLE_WORLD_URL + url_template)
+#     assert res.status_code == 200
+#
+#
+# if __name__ == "__main__":
+#     unittest.main()
