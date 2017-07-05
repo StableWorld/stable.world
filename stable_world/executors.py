5e11aff417e2d5907f3626472fa25a7e9ae4414c
Sean Ross-Ross <srossross@gmail.com>
2017-07-05 07:28:34 -0700

    adding npm

--- stable_world/executors.py
+++ stable_world/executors.py
@@ -1,8 +1,12 @@
+from __future__ import print_function, unicode_literals, absolute_import
+
 import sys
 import os
-from stable_world import errors
+from base64 import b64encode
 from subprocess import check_call, CalledProcessError
 from logging import getLogger
+
+from stable_world import errors
 
 logger = getLogger(__name__)
 
@@ -27,9 +31,43 @@ def execute_pip(app, bucket_name, pip_args):
         os.makedirs(env['PIP_CACHE_DIR'])
 
     logger.debug('Executing {}'.format(' '.join(args)))
-    print("call")
     try:
         check_call(args, stdout=sys.stdout, stderr=sys.stderr, env=env)
     except CalledProcessError:
         # User friendly error
         raise errors.UserError("Command pip failed")
+
+
+def write_npm_config(fd, obj):
+    for key, value in obj.items():
+        print(key, value, sep='=', file=fd)
+    return
+
+
+def execute_npm(app, bucket_name, npm_args):
+
+    token = app.token
+    app.client.check_bucket_token(bucket_name, token)
+    args = ['npm'] + list(npm_args)
+
+    env = os.environ.copy()
+    env['NPM_CONF_USERCONFIG'] = './.tmp-npm-config'
+
+    npm_config = {}
+    npm_config['always-auth'] = 'true'
+    npm_config['registry'] = '{url}/cache/{bucket}/npm/'.format(
+        url=app.config['url'], bucket=bucket_name,
+    )
+    # TODO: implement me
+    npm_config['_auth'] = b64encode('token:{}'.format(token).encode()).decode()
+
+    with open(env['NPM_CONF_USERCONFIG'], 'w') as fd:
+        write_npm_config(fd, npm_config)
+
+    logger.debug('Executing {}'.format(' '.join(args)))
+
+    try:
+        check_call(args, stdout=sys.stdout, stderr=sys.stderr, env=env)
+    except CalledProcessError:
+        # User friendly error
+        raise errors.UserError("Command npm failed")
