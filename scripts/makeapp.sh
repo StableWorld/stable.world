b4809031724a912067e826152024ad36e6f68b9b
Sean Ross-Ross <srossross@gmail.com>
2017-08-26 13:50:39 -0700

    fix python

--- scripts/makeapp.sh
+++ scripts/makeapp.sh
@@ -1,13 +1,13 @@
 set -euo pipefail
 
 git clean -xdf dist app
-python3 setup.py sdist
+python3.4 setup.py sdist
 pip install --target app dist/stable.world-*.tar.gz
 
-python2 -m compileall app/
-python3 -m compileall app/
+python2.7 -m compileall app/
+python3.4 -m compileall app/
 
-python3 -m zipapp app -o ./bin/stable.world.pyz
+python3.4 -m zipapp app -o ./bin/stable.world.pyz
 cat ./app/header.sh ./bin/stable.world.pyz > ./bin/stable.world
 cat ./app/header.bat ./bin/stable.world.pyz > ./bin/stable.world.bat
 chmod +x ./bin/stable.world
