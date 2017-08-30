1aded1c4dbffa9ab28084c537cae527ad5ed8a77
Sean Ross-Ross <srossross@gmail.com>
2017-08-30 07:36:34 -0700

    refactor command into their own files

---
+++ stable_world/commands/misc.py
@@ -0,0 +1,30 @@
+
+import click
+from stable_world import utils, output, group, application
+
+
+@click.group(cls=group.StableWorldGroup)
+def main():
+    pass
+
+
+@main.command()
+@utils.login_optional
+def info(app):
+    "Fetch environment and server informations"
+    output.build_info.build_info(app.client)
+
+
+@click.argument('key')
+@click.argument('value')
+@application.pass_app
+def set(app, key, value):
+    """Set a config value
+
+    e.g:
+
+    """
+    app.write_config(key, value)
+    utils.echo_success()
+    click.echo('key "{}" was set'.format(key))
+    return
