305bed3dc2da569c530fafe0226ea50965badacd
Sean Ross-Ross <srossross@gmail.com>
2018-01-07 12:37:35 -0800

    change script to run python in docker

--- scripts/makeapp.sh
+++ scripts/makeapp.sh
@@ -1,13 +1,23 @@
-set -eu
+set -eux
 
 git clean -xdf dist app
-python3.6 setup.py sdist
-pip install --target app dist/stable.world-*.tar.gz
 
-python2.7 -m compileall app/
-python3.6 -m compileall app/
+docker run -v $(pwd):/sw -w /sw circleci/python:3.6 \
+  python setup.py sdist
 
-python3.6 -m zipapp app -o ./bin/stable.world.pyz
+docker run -v $(pwd):/sw -w /sw circleci/python:3.6 \
+  pip install --target app dist/stable.world-*.tar.gz
+
+docker run -v $(pwd):/sw -w /sw circleci/python:2.7 \
+  python -m compileall app/
+
+docker run -v $(pwd):/sw -w /sw circleci/python:3.6 \
+  python -m compileall app/
+
+docker run  -v $(pwd):/sw -w /sw circleci/python:3.6 \
+  python -m zipapp app -o ./bin/stable.world.pyz
+
 cat ./app/header.sh ./bin/stable.world.pyz > ./bin/stable.world
-cat ./app/header.bat ./bin/stable.world.pyz > ./bin/stable.world.bat
+# TODO: windows deploy
+# cat ./app/header.bat ./bin/stable.world.pyz > ./bin/stable.world.bat
 chmod +x ./bin/stable.world
