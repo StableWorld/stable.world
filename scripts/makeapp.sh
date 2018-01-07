a270d561d352ddaa5a55235ee90507a3ac28cc39
Sean Ross-Ross <srossross@gmail.com>
2018-01-07 13:19:58 -0800

    fix ci

--- scripts/makeapp.sh
+++ scripts/makeapp.sh
@@ -4,29 +4,5 @@ git clean -xdf dist app
 docker build -f scripts/Dockerfile -t makeapp:build .
 docker create --name extract makeapp:build
 docker cp extract:/project/bin/stable.world ./bin/stable.world
+ls -la bin/stable.world
 echo "OK!"
-
-# docker rm -f extract
-
-# docker run -v $(pwd):/sw -w /sw circleci/python:3.6 \
-#   ls -al
-#
-# docker run -v $(pwd):/sw -w /sw circleci/python:3.6 \
-#   python setup.py sdist
-#
-# docker run -v $(pwd):/sw -w /sw circleci/python:3.6 \
-#   pip install --target app dist/stable.world-*.tar.gz
-#
-# docker run -v $(pwd):/sw -w /sw circleci/python:2.7 \
-#   python -m compileall app/
-#
-# docker run -v $(pwd):/sw -w /sw circleci/python:3.6 \
-#   python -m compileall app/
-#
-# docker run  -v $(pwd):/sw -w /sw circleci/python:3.6 \
-#   python -m zipapp app -o ./bin/stable.world.pyz
-#
-# cat ./app/header.sh ./bin/stable.world.pyz > ./bin/stable.world
-# # TODO: windows deploy
-# # cat ./app/header.bat ./bin/stable.world.pyz > ./bin/stable.world.bat
-# chmod +x ./bin/stable.world
